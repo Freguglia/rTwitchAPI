8a47fb0c8970682779540664a982d5446093738a
Victor Freguglia <victorfreguglia@gmail.com>
2019-03-21 09:08:23 -0300

    Update get_users.R

--- R/get_users.R
+++ R/get_users.R
@@ -2,7 +2,6 @@
 #' Gets information about one or more specified Twitch users. Users are identified by optional user IDs and/or login name. If neither a user ID nor a login name is specified, the user is looked up by Bearer token.
 #' @param user_id User ID. Multiple user IDs can be specified. Limit: 100.
 #' @param user_login User login name. Multiple login names can be specified. Limit: 100.
-#' @return A list with clip information.
 #' @export
 #' @references https://dev.twitch.tv/docs/api/reference/#get-users
 
