7f6fd42af896e9ee9b4f2cf860189a3b07232e26
Freguglia <victorfreguglia@gmail.com>
2019-09-12 17:26:07 -0300

    Put authenticantion functions in the same .Rd file.

--- R/authentication.R
+++ R/authentication.R
@@ -1,21 +1,17 @@
 #' Authentication
 #' 
-#' Sets up your Client-ID.
-#' @param clientID your client-ID.
+#' Sets up your Client-ID or forgets it.
+#' @name authentication
+#' @param client_id your client-ID.
 #' @references https://dev.twitch.tv/docs/authentication/
 #' @details If, for some reason, you need to change what client-ID you're making calls with, use use \code{\link{twitch_auth_forget}} and then reuse this function to set the new client-ID. Check the link in references to understand what is a client-ID, how it works and how to get one.
 #' @export
-
 twitch_auth = function(client_id){
   httr::set_config(httr::add_headers('Client-ID' = client_id))
 }
 
-#' Reset Authenctication
-#' 
-#' Forgets Client-ID.
-#' @details Use this to forget the client-ID previously set, then set your new client-ID with \code{\link{twitch_auth}}
+#' @rdname authentication
 #' @export
-
 twitch_auth_forget = function(){
   httr::reset_config()
 }
